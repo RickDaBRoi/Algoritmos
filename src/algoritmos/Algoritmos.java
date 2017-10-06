e34bf958b3b29f742a0cbcc7d0f93e82a4dd1ac2
Rick Dabroi <Rick Dabroi@172.21.155.44>
2017-10-05 21:55:41 -0300

    Criação do Projeto no NetBeans

--- src/algoritmos/Algoritmos.java
+++ src/algoritmos/Algoritmos.java
@@ -1,9 +1,21 @@
 package algoritmos;
 
 public class Algoritmos {
-
-    public static void main(String[] args) {
-        
-    }
     
+    public static void quadrado(double n) {
+        double r = n * n;
+        System.out.println("Quadrado: " + r);
+    }
+    public static double metade(double v) {
+        double r = v / 2;
+        return r;
+    }
+    public static void main(String[] args) {
+                        
+        System.out.println("A raiz é: " + Math.sqrt(25));
+        
+        quadrado(10);
+                
+        System.out.println("Metade" + metade(15));        
+    }    
 }
